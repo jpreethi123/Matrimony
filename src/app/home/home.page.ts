e66a0159d10e4786aca19f026a25dc970c984ae8
Rink08 <rinkesh.kore35@gmail.com>
2021-04-14 23:16:28 +0530

    signin-signup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('slidesRef') viewer: IonSlides;
+
+  slideOpts = {
+    initialSlide: 1,
+    speed: 500,
+    autoplay: true
+  };
 
   constructor() {}
+
+  onSlideMoved(event) {
+    /** isEnd true when slides reach at end slide */
+    event.target.isEnd().then(isEnd => {
+      console.log('End of slide', isEnd);
+    });
+
+    event.target.isBeginning().then((istrue) => {
+      console.log('End of slide', istrue);
+    });
+  }
+
 
 }
